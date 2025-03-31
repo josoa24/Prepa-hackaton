4b2b3c02222d3334aef365bfd0df4d2f30042b1f
Josoa <167620799+josoa24@users.noreply.github.com>
2025-03-31 09:47:30 +0300

    Login

---
+++ sql/base.sql
@@ -0,0 +1,22 @@
+create database i_colab;
+
+use i_colab;
+
+create table
+    i_colab_user (
+        user_id INT AUTO_INCREMENT PRIMARY KEY,
+        last_name VARCHAR(100) NOT NULL,
+        first_name VARCHAR(100) NOT NULL,
+        address TEXT,
+        country VARCHAR(50),
+        email VARCHAR(100) UNIQUE NOT NULL,
+        phone_number VARCHAR(20),
+        password VARCHAR(255) NOT NULL
+    );
+
+
+    insert into i_colab_user (last_name, first_name, address, country, email, phone_number, password)
+    values 
+    ('Doe', 'John', '123 Main St', 'USA', 'john.doe@example.com', '123-456-7890', 'password123'),
+    ('Smith', 'Jane', '456 Elm St', 'Canada', 'jane.smith@example.com', '987-654-3210', 'securepass456'),
+    ('Brown', 'Charlie', '789 Oak St', 'UK', 'charlie.brown@example.com', '555-555-5555', 'mypassword789');
